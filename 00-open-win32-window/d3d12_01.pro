6f64d71d41c03a5e2f069fdb924b8d9c21a15c39
Matt Chang <chchwy@gmail.com>
2020-09-04 16:38:44 +1000

    Initial commit

---
+++ 00-open-win32-window/d3d12_01.pro
@@ -0,0 +1,8 @@
+CONFIG -= qt
+QT -= core gui
+
+TEMPLATE = app
+TARGET = DX12_01
+SOURCES = main.cpp
+
+PRECOMPILED_HEADER = pch.h
---
+++ 00-open-win32-window/pch.h
@@ -0,0 +1,9 @@
+
+#define WIN32_LEAN_AND_MEAN
+#define NOMINMAX
+#include <windows.h>
+
+#include <iostream>
+#include <functional>
+#include <memory>
+
